250f143c9aaad6c76b0894754408e69b76723b0b
Daniel Riezebos <daniel@marissen.net>
2020-12-14 11:36:31 +0100

    Added API username/password fields

--- Api/Data/SiteInterface.php
+++ Api/Data/SiteInterface.php
@@ -13,6 +13,8 @@ interface SiteInterface extends ExtensibleDataInterface
     const NAME = 'name';
     const ENABLED = 'enabled';
     const SITE_ID = 'site_id';
+    const API_USERNAME = 'api_username';
+    const API_PASSWORD = 'api_password';
 
     /**
      * Get site_id
@@ -78,4 +80,30 @@ interface SiteInterface extends ExtensibleDataInterface
      * @return SiteInterface
      */
     public function setEnabled($enabled);
+
+    /**
+     * Get api_username
+     * @return string
+     */
+    public function getApiUsername(): string;
+
+    /**
+     * Set api_username
+     * @param string $apiUsername
+     * @return SiteInterface
+     */
+    public function setApiUsername(string $apiUsername);
+
+    /**
+     * Get api_password
+     * @return string
+     */
+    public function getApiPassword(): string;
+
+    /**
+     * Set api_password
+     * @param string $apiPassword
+     * @return SiteInterface
+     */
+    public function setApiPassword(string $apiPassword);
 }
